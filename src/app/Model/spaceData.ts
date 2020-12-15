8bb441eb7c883f3efd9a68cfd2193910cf2b6868
parasmittal021 <ratanprojects021@gmail.com>
2020-12-15 08:43:21 +0530

    Some changes

--- src/app/Model/spaceData.ts
+++ src/app/Model/spaceData.ts
@@ -32,27 +32,7 @@ export class SpaceDataModel {
         this.launchYear = spaceData?.launch_year;
         this.launchSuccess = spaceData?.launch_success;
         this.landSuccess = spaceData?.rocket?.first_stage?.cores?.every(el => el?.land_success);
-        this.missionPatch = this.cloudinarize(spaceData?.links?.mission_patch_small || spaceData?.links?.mission_patch);
+        this.missionPatch = spaceData?.links?.mission_patch_small || spaceData?.links?.mission_patch;
     }
-    private cloudinarize(rawUrl: string, cloudName = 'dw9ojz2hi', folder = 'v1605185972/xt-media',
-    optimizations= 'ar_1:1,c_fill/c_scale,w_auto,dpr_auto,q_auto,f_auto') {
-const mappedPrefix = 'https://images2.imgbox.com/';
-let url = '';
-let path = '';
-if (rawUrl?.startsWith(mappedPrefix)) {
-path = rawUrl.replace(mappedPrefix, '');
-url = 'https://res.cloudinary.com/{{cloudName}}/image/upload/{{optimizations}}/{{folder}}/{{path}}';
-} else if (rawUrl) {
-path = rawUrl;
-url = 'https://res.cloudinary.com/{{cloudName}}/image/fetch/{{optimizations}}/{{path}}';
-} else {
-folder = 'v1605185972';
-url = 'https://res.cloudinary.com/{{cloudName}}/image/upload/{{optimizations}}/{{folder}}/loader.jpg';
-}
-return url.replace('{{cloudName}}', cloudName)
-.replace('{{folder}}', folder)
-.replace('{{optimizations}}', optimizations)
-.replace('{{path}}', path);
-}
-  
+
 }
